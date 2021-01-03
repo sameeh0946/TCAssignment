95da98cfd3366ed9b3d6701194be92d27b35b359
Sameeh <abd.sam0946@gmail.com>
2021-01-04 00:19:50 +0530

    New files added

---
+++ TCAssignment/Utils/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  TCAssignment
+//
+//  Created by www.abcom.in on 04/01/21.
+//  Copyright © 2021 Sameeh Ahmed. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let blogUrl = "https://truecaller.blog/2018/03/15/how-to-become-an-ios-developer/"
+    static let titleMessage = "Warning! There was a network error"
+    static let descMessage = "Please try again"
+    static let okMessage = "Ok"
+    static let tryMessage = "Please try again"
+}
--- TCAssignment/ViewController.swift
+++ TCAssignment/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
     }
     
     func scrapeTCBlog() -> Void {
-        AF.request("https://truecaller.blog/2018/03/15/how-to-become-an-ios-developer/").responseString { response in
+        AF.request(Constants.blogUrl).responseString { response in
             //print("check here: \(String(describing: response.value))")
 
             guard let html = response.value else {
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
         
         guard let strippedHtmlString = html.stripOutHtml() else{return}
         self.taskButton.loadingIndicator(show: false)
-        //3
+        //Task 3
         let arrayOfRemovedWhitespaces = strippedHtmlString.removingWhitespaces()
         var newRemovedEmptyElements = [String]()
         for elements in arrayOfRemovedWhitespaces {
@@ -92,72 +92,3 @@ class ViewController: UIViewController {
 
 
 }
-
-extension String {
-
-    func stripOutHtml() -> String? {
-        do {
-            guard let data = self.data(using: String.Encoding(rawValue: String.Encoding.utf8.rawValue)) else {
-                return nil
-            }
-            let attributed = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
-            return attributed.string
-        } catch {
-            return nil
-        }
-    }
-    
-    func removingWhitespaces() -> [String] {
-        return components(separatedBy: .whitespacesAndNewlines)
-    }
-    
-    func removingWhitespacesString() -> String {
-        return components(separatedBy: .whitespacesAndNewlines).joined()
-    }
-    
-    subscript(i: Int) -> String {
-        return String(self[index(startIndex, offsetBy: i)])
-    }
-    
-}
-
-
-extension UIButton {
-    func loadingIndicator(show: Bool) {
-        if show {
-            print("Utils showloadingIndicator")
-            self.setTitle("Signing up ...", for: .disabled)
-            let indicator = UIActivityIndicatorView()
-            let buttonHeight = self.bounds.size.height
-            let buttonWidth = self.bounds.size.width
-            indicator.center = CGPoint(x: buttonWidth-20, y: buttonHeight/2)
-            self.addSubview(indicator)
-            indicator.startAnimating()
-        } else {
-            for view in self.subviews {
-                print("Utils showfalse loadingIndicator")
-                if let indicator = view as? UIActivityIndicatorView {
-                    indicator.stopAnimating()
-                    indicator.removeFromSuperview()
-                }
-            }
-        }
-    }
-}
-
-extension UIViewController {
-    func showAlert(title: String = "Warning! There was a network error",
-                   message: String = "Please try again" ,
-                   buttonTitle: String = "Ok",
-                   buttonHandler: ((UIAlertAction) -> Void)? = nil) {
-        let alert = UIAlertController(title: title,
-                                      message: "Please try again",
-                                      preferredStyle: .alert)
-        let action = UIAlertAction(title: buttonTitle,
-                                   style: .default,
-                                   handler: buttonHandler)
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
-    }
-}
-
