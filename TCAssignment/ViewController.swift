214c7fa7375f1a036a126c754c79696842ccfb2b
Sameeh <abd.sam0946@gmail.com>
2021-01-03 22:16:28 +0530

    UI remains

--- TCAssignment/ViewController.swift
+++ TCAssignment/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Alamofire
-//import Kanna
+
 
 class ViewController: UIViewController {
 
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         button.addTarget(self, action: #selector(ViewController.buttonTapped), for: .touchUpInside)
+        
+
     }
     
     @objc func buttonTapped(){
@@ -37,11 +39,32 @@ class ViewController: UIViewController {
     }
 
     func parseHTML(html: String) -> Void {
-        print("parseHTML called")
-        let str = html.stripOutHtml()
-        //let str = html.withoutHtmlTags()
-        // Finish this next
-        print("str: \(String(describing: str))")
+        guard let strippedHtmlString = html.stripOutHtml() else{return}
+    
+        //3
+        let arrayOfRemovedWhitespaces = strippedHtmlString.removingWhitespaces()
+        var newRemovedEmptyElements = [String]()
+        for elements in arrayOfRemovedWhitespaces {
+            if elements.count > 0{
+                newRemovedEmptyElements.append(elements)
+            }
+        }
+        print("lastRequest: \(newRemovedEmptyElements.count)")
+        
+        //2
+        let stringOfRemovedWhiteSpaces = strippedHtmlString.removingWhitespacesString()
+        var i = 9
+        var n10thArrays = [String]()
+        while i < stringOfRemovedWhiteSpaces.count {
+            n10thArrays.append(stringOfRemovedWhiteSpaces[i])
+            i = i + 9
+        }
+        print("secondArrayTask \(n10thArrays)")
+        
+        
+        //1
+        let first10thChar = strippedHtmlString[9]
+        print("first10thChar \(first10thChar)")
     }
 
 
@@ -52,7 +75,7 @@ extension String {
 
     func stripOutHtml() -> String? {
         do {
-            guard let data = self.data(using: .unicode) else {
+            guard let data = self.data(using: String.Encoding(rawValue: String.Encoding.utf8.rawValue)) else {
                 return nil
             }
             let attributed = try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
@@ -61,4 +84,18 @@ extension String {
             return nil
         }
     }
+    
+    func removingWhitespaces() -> [String] {
+        return components(separatedBy: .whitespacesAndNewlines)
+    }
+    
+    func removingWhitespacesString() -> String {
+        return components(separatedBy: .whitespacesAndNewlines).joined()
+    }
+    
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+    
 }
+
