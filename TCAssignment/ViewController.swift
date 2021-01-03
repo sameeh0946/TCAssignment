3d0c4c68135e40bdddd326ccf430300e22081e73
Sameeh <abd.sam0946@gmail.com>
2021-01-03 08:36:23 +0530

    fresh commit

---
+++ TCAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TCAssignment
+//
+//  Created by Sameeh Ahmed on 12/30/20.
+//  Copyright © 2020 Sameeh Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
